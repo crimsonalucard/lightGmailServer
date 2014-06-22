8556e4b18ed319380fa458b3754cd1dc0f1e97c2
Brian Yeh <heynairb@gmail.com>
2014-06-22 01:15:13 -0700

    compiling...

--- herokupush.sh
+++ herokupush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-heroku config:set user  =   $(jq '.user' emailLogin.json | sed -e 's/\"/')
-heroku config:set pass  =   $(jq '.pass' emailLogin.json | sed -e 's/\"/')
-heroku config:set to    =   $(jq '.to' emailLogin.json | sed -e 's/\"/')
+heroku config:set user  =   $(jq '.user' emailLogin.json | sed -e 's/\"//')
+heroku config:set pass  =   $(jq '.pass' emailLogin.json | sed -e 's/\"//')
+heroku config:set to    =   $(jq '.to' emailLogin.json | sed -e 's/\"//')
 closure-compiler email-server.js > email-server.min.js
 git commit -am "compiling..."
 git push heroku master
